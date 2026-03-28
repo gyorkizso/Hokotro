fd01a26516561874cce2e8b7ffa0ecc4c87d9354
GlizzyHacker <saxattila@gmail.com>
2026-03-28 16:35:21 +0100

    test method signatures

--- src/Skeleton.java
+++ src/Skeleton.java
@@ -1,3 +1,86 @@
 public class Skeleton {
-    
+    public void testMatchStart(){
+
+    }
+
+    public void testSnowfall() {
+
+    }
+
+    public void tesIcing() {
+
+    }
+
+    public void testVehicleMoving() {
+
+    }
+
+    public void testUsingBroomHead() {
+
+    }
+
+    public void testUsingThrowerHead() {
+
+    }
+
+    public void testUsingIceBreakerHead() {
+
+    }
+
+    public void testUsingSaltSpreaderHead() {
+
+    }
+
+    public void testUsingDragonHead() {
+
+    }
+
+    public void testCollisionHandling() {
+
+    }
+
+    public void testObstacleRemoval(){
+
+    }
+
+    public void testPlowHeadChange() {
+
+    }
+
+    public void testCarRouteSelection() {
+
+    }
+
+    public void testLaneChangeOnObstacle() {
+
+    }
+
+    public void testCompleteBusRoute(){
+
+    }
+
+
+    public void testVisitBusStop() {
+
+    }
+
+    public void testCleanWithSnowPlow() {
+
+    }
+
+    public void testVehicleStuckInLane() {
+
+    }
+
+    public void testPurchaseSnowPlowInGarage() {
+
+    }
+
+    public void testFreeVehicle() {
+
+    }
+
+    public void testCleanerPoints() {
+
+    }
 }
