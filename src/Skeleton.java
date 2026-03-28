bd4118e8a1019eb2c2270570aac53ca477c35a65
GlizzyHacker <saxattila@gmail.com>
2026-03-28 19:18:00 +0100

    user input methods

--- src/Skeleton.java
+++ src/Skeleton.java
@@ -1,4 +1,53 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Skeleton {
+    Scanner scanner;
+
+    Skeleton(){
+        scanner = new Scanner(System.in);
+    }
+
+    String getStringFromUser(String paramName, String extra){
+        System.out.printf("Kérem adja meg a(z) %s értékét %s:", paramName, extra);
+        String string = scanner.nextLine();
+        return string.strip();
+    }
+
+    String getStringFromUser(String paramName){
+        return getStringFromUser(paramName, "");
+    }
+
+    int getIntFromUser(String paramName) {
+        String raw = getStringFromUser(paramName);
+        return Integer.parseInt(raw);
+    }
+
+    double getDoubleFromUser(String paramName) {
+        String raw = getStringFromUser(paramName);
+        return Double.parseDouble(raw);
+    }
+
+    boolean getBooleanFromUser(String paramName) {
+        String raw = getStringFromUser(paramName, "(0=hamis, 1=igaz):");
+        switch (raw){
+            case "0":
+                return false;
+            case "1":
+                return true;
+            default:
+                throw new NumberFormatException();
+        }
+    }
+
+    int getListSelectionFromUser(List<String> options){
+        for (int i = 0; i < options.size(); i++){
+            System.out.printf("%i. %s%n", i, options.get(i));
+        }
+        System.out.print("Kérem válasszon a fentiek közül:");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
     public void testMatchStart(){
 
     }
