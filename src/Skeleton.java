7d965b2d4071917c13cd5e33196bbcca280e438d
Attila Sax <75539035+GlizzyHacker@users.noreply.github.com>
2026-03-30 10:42:35 +0200

    Kommentek

--- src/Skeleton.java
+++ src/Skeleton.java
@@ -3,10 +3,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-
+/**
+* Singleton osztály a Skeleton teszteseteinek, valamint ki és bemenetének kezelésére
+*/
 public class Skeleton {
+    /** A singleton példány */
     public static Skeleton instance;
+    /** A tesztek során létrehozott objektumok neveit tartja számon */
     public Map<Object, String> names = new HashMap<>();
+    
     Scanner scanner;
 
     Skeleton(){
@@ -14,26 +19,53 @@ public class Skeleton {
         instance = this;
     }
 
+    /** Bekér a felhasználótól egy Stringet.
+    *
+    * Az egész sort beolvassa szóközökkel együtt.
+    * @param paramName a paraméter neve amihez kérjük az értéket.
+    * @param extra extra szöveg amit a normál üzenet után ír.
+    * @return a felhasználó által megadott szöveg
+    */
     String getStringFromUser(String paramName, String extra){
         System.out.printf("Kérem adja meg a(z) %s értékét %s:", paramName, extra);
         String string = scanner.nextLine();
         return string.strip();
     }
 
+    /** Bekér a felhasználótól egy szöveget
+    *
+    * Az egész sort beolvassa szóközökkel együtt.
+    * @param paramName a paraméter neve amihez kérjük az értéket.
+    * @return a felhasználó által megadott szöveg
+    */
     String getStringFromUser(String paramName){
         return getStringFromUser(paramName, "");
     }
 
+    /** Bekér a felhasználótól egy egész számot
+    * @param paramName a paraméter neve amihez kérjük az értéket.
+    * @return a felhasználó által megadott szám
+    */
     int getIntFromUser(String paramName) {
         String raw = getStringFromUser(paramName);
         return Integer.parseInt(raw);
     }
 
+    /** Bekér a felhasználótól egy valós számot
+    * @param paramName a paraméter neve amihez kérjük az értéket.
+    * @return a felhasználó által megadott szám
+    */
     double getDoubleFromUser(String paramName) {
         String raw = getStringFromUser(paramName);
         return Double.parseDouble(raw);
     }
 
+    /** Bekér a felhasználótól egy igaz hamis értéke
+    
+    * 0 jelenti az igazat és 1 a hamisat. Egyéb érték hibát dob.
+    * @param paramName a paraméter neve amihez kérjük az értéket.
+    * @return a felhasználó által megadott érték
+    */
     boolean getBooleanFromUser(String paramName) {
         String raw = getStringFromUser(paramName, "(0=hamis, 1=igaz):");
         switch (raw){
@@ -46,6 +78,12 @@ public class Skeleton {
         }
     }
 
+    /** Megkéri a felhasználót, hogy válasszon a megadott opciók közül
+    
+    * 0 jelenti az igazat és 1 a hamisat. Egyéb érték hibát dob.
+    * @param options az opciók amik fel lesznek sorolva.
+    * @return a felhasználó által választott opció sorszáma
+    */
     int getListSelectionFromUser(List<String> options){
         for (int i = 0; i < options.size(); i++){
             System.out.printf("%d. %s%n", i, options.get(i));
@@ -54,7 +92,12 @@ public class Skeleton {
         return Integer.parseInt(scanner.nextLine());
     }
 
-
+    /** Kiírja standard kimenetre egy metódus meghívását
+    * 
+    * @param o az objektum amihez tartozik a metódus
+    * @param methodName a metódus neve
+    * @param args a metódus argumentumainak nevei és értékei. A neveket és értékeket egymás után kell megadni felváltva, kezdve az első argumentum nevével.
+    */
     public void methodCall(Object o, String methodName, Object... args){
         System.out.printf("%s.%s(", names.get(o), methodName);
         for (int j = 0; j < args.length; j+=2) {
@@ -65,7 +108,11 @@ public class Skeleton {
         System.out.print(") meghívva\n");
     }
 
-
+    /** Kiírja standard kimenetre egy objektum létrehozását
+    * 
+    * @param o az objektum ami létrejött
+    * @param args a konstruktor argumentumainak nevei és értékei. A neveket és értékeket egymás után kell megadni felváltva, kezdve az első argumentum nevével.
+    */
     public void createObject(Object o, Object... args) {
         String type = o.getClass().getTypeName();
         int i = 1;
@@ -87,7 +134,12 @@ public class Skeleton {
         System.out.print("\n");
     }
 
-
+    /** Kiírja standard kimenetre egy metódus visszatérését
+    * 
+    * @param o az objektum amihez tartozik a metódus
+    * @param methodName a metódus neve
+    * @param returnValue a metódus visszatérési értéke
+    */
     public void methodReturn(Object o, String methodName, Object returnValue) {
         System.out.printf("%s.%s visszatért", names.get(o), methodName);
         if (returnValue != null){
@@ -96,6 +148,11 @@ public class Skeleton {
         System.out.print("\n");
     }
 
+    /** Kiírja standard kimenetre egy metódus visszatérését
+    * 
+    * @param o az objektum amihez tartozik a metódus
+    * @param methodName a metódus neve
+    */
     public void methodReturn(Object o, String methodName) {
         methodReturn(o, methodName, null);
     }
